500dba313dbf6cc57b800d87a6f53c5357311c71
xian <ch66971542@gmail.com>
2023-12-12 20:00:21 +0800

    創建專案-初始化

---
+++ src/test/java/com/chenxian/language_platform/LanguagePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenxian.language_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
